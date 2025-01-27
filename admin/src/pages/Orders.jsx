96e0d01b2608b9400804ac1eb37133bc21405503
Bhavrattan Singh Bagga <110535685+rattan004@users.noreply.github.com>
2025-01-27 09:30:29 +0530

    Orders.jsx

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -16,7 +16,7 @@ const Orders = ({ token }) => {
     try {
       const response = await axios.post(backendUrl + '/api/order/list', {}, { headers: { token } })
       if (response.data.success) {
-        setOrders(response.data.orders.reverse)
+        setOrders(response.data.orders)
       }
       else {
         toast.error(response.data.message)
