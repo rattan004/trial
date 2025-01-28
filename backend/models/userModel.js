63db9c4bb7dc56fb455dd9dc6d43a55ea25ec8ae
Bhavrattan Singh Bagga <110535685+rattan004@users.noreply.github.com>
2025-01-28 18:52:04 +0530

    userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name:{type:String, required:true},
     email:{type:String, required:true,unique:true},
+    course:{type:String, required:true},
+    section:{type:String, required:true},
+    roll:{type:String, required:true,unique:true},
     password:{type:String, required:true},
     cartData:{type:Object, default: {}}
 },{minimize:false})
