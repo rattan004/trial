06f5f8c534388e4dd201ce6442a89a3d783ef8bb
Bhavrattan Singh Bagga <110535685+rattan004@users.noreply.github.com>
2025-02-01 04:52:38 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     email:{type:String, required:true,unique:true},
     course:{type:String, required:true},
     section:{type:String, required:true},
+    semester:{type:Number, required:true},
     roll:{type:String, required:true,unique:true},
     password:{type:String, required:true},
     cartData:{type:Object, default: {}}
