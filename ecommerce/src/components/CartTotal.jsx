4a8822c62277b3e9f41f19eeaea60406835f7ea6
Bhavrattan Singh Bagga <110535685+rattan004@users.noreply.github.com>
2025-01-31 18:37:53 +0530

    CartTotal.jsx

--- ecommerce/src/components/CartTotal.jsx
+++ ecommerce/src/components/CartTotal.jsx
@@ -11,27 +11,13 @@ const CartTotal = () => {
         <Title text1={'CART'} text2={' TOTAL'} />
       </div>
 
-      <div className="flex flex-col gap-2 mt-2 text-sm ">
-        <div className="flex justify-between">
-          <p>Subtotal</p>
-          <p>
-            {' '}
-            {currency} {getCartAmount()}.00
-          </p>
-        </div>
-        <div className="flex justify-between">
-          <p>Shipping Fee</p>
-          <p>
-            {' '}
-            {currency} {delivery_fee}.00
-          </p>
-        </div>
+      <div className="flex flex-col gap-2 mt-2 text-md ">
         <div className="flex justify-between">
           <b>Total</b>
           <b>
             {' '}
             {currency}{' '}
-            {getCartAmount() === 0 ? 0 : getCartAmount() + delivery_fee}.00
+            {getCartAmount() === 0 ? 0 : getCartAmount()}.00
           </b>
         </div>
       </div>
