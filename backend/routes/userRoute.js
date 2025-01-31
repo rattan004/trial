8e2f871336dd85d2520e8fe6c07ed567c4ea0e86
Bhavrattan Singh Bagga <110535685+rattan004@users.noreply.github.com>
2025-02-01 04:53:56 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { loginUser,registerUser,adminLogin } from '../controllers/userController.js';
+import { loginUser,registerUser,adminLogin, userDetails, updateUserDetails } from '../controllers/userController.js';
+import authUser from '../middleware/auth.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 userRouter.post('/admin',adminLogin)
+userRouter.post('/details',authUser,userDetails)
+userRouter.post('/update',authUser,updateUserDetails)
 
 export default userRouter;
