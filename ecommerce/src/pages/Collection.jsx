e7235e5bb89f2d702d7e4c25eb17c35c5d123859
Bhavrattan Singh Bagga <110535685+rattan004@users.noreply.github.com>
2025-02-01 10:56:13 +0530

    Update Collection.jsx

--- ecommerce/src/pages/Collection.jsx
+++ ecommerce/src/pages/Collection.jsx
@@ -102,7 +102,7 @@ const Collection = () => {
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
           {
             filterProducts.map((item,index)=>(
-              <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image} />
+              <ProductItem key={index} id={item._id} name={item.name} price={item.price} stock={item.stock} image={item.image} />
             ))
           }
         </div>
