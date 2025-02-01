0f905286d56097b66d4a74f7aba52704dbe5cbd0
Bhavrattan Singh Bagga <110535685+rattan004@users.noreply.github.com>
2025-02-01 10:55:41 +0530

    Update ProductItem.jsx

--- ecommerce/src/components/ProductItem.jsx
+++ ecommerce/src/components/ProductItem.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom';
 
-const ProductItem = ({id,image,name,price}) => {
+const ProductItem = ({id,image,name,stock,price}) => {
     
     const {currency}= useContext(ShopContext);
     
@@ -12,6 +12,7 @@ const ProductItem = ({id,image,name,price}) => {
             <img src={image[0]} className='hover:scale-110 transistion ease-in-out' alt="Product Image" />
             <p className='pt-3 pb-1 text-sm'>{name}</p>
             <p className='text-sm font-medium'>{currency}{price}</p>
+            {stock === 0 || stock < 0 ? <p className='bg-red-700'>Out Of Stock</p>:<p className='bg-green-500 text-white inline-block mt-2 rounded-2xl  p-2'>In Stock</p>}
         </div>
     </Link>
   )
