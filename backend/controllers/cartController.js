d023f4e65b8c137e4076e2580ea8e264b2fd6e07
Bhavrattan Singh Bagga <110535685+rattan004@users.noreply.github.com>
2025-02-01 10:28:08 +0530

    Update cartController.js

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,4 +1,5 @@
 
+import productModel from "../models/productModel.js";
 import userModel from "../models/userModel.js"
 
 // add products to user cart
@@ -6,18 +7,22 @@ const addToCart = async (req,res) => {
     try {
         const {userId, itemId, size} = req.body;
         const userData = await userModel.findById(userId);
+        const item = await productModel.findById(itemId)
         let cartData = await userData.cartData;
         if (cartData[itemId]) {
             if(cartData[itemId][size]){
                 cartData[itemId][size]+=1
+                await productModel.findByIdAndUpdate(item._id,{stock:item.stock-1})
             }
             else{
                 cartData[itemId][size]=1
+                await productModel.findByIdAndUpdate(item._id,{stock:item.stock-1})
             }
         }
         else{
             cartData[itemId] = {}
             cartData[itemId][size] = 1
+            await productModel.findByIdAndUpdate(item._id,{stock:item.stock-1})
         }
         await userModel.findByIdAndUpdate(userId,{cartData})
         res.json({success:true , message: "Added to Cart"})
@@ -30,13 +35,23 @@ const addToCart = async (req,res) => {
 //update
 const updateCart = async(req,res) =>{
     try {
-        const {userId,itemId,size,quantity} = req.body
+        const {userId,itemId,size,quantity,change} = req.body
         const userData = await userModel.findById(userId);
         let cartData = await userData.cartData;
-
+        const item = await productModel.findById(itemId)
         cartData[itemId][size] = quantity
-        await userModel.findByIdAndUpdate(userId,{cartData})
-        res.json({success:true, message:"Cart Updated"})
+        const newStock = item.stock - change;
+        if (newStock >= 0) { // Stock can't go negative
+            cartData[itemId] = cartData[itemId] || {}; // Ensure the itemId exists in the cartData object. Prevents errors
+            cartData[itemId][size] = quantity;
+
+            await productModel.findByIdAndUpdate(item._id, { stock: newStock });
+            await userModel.findByIdAndUpdate(userId, { cartData });
+
+            res.json({ success: true, message: "Cart Updated" });
+        } else {
+            res.json({ success: false, message: "Not enough stock" }); // More descriptive message
+        }
 
     } catch (error) {
         res.json({success:false, message:error.message})
