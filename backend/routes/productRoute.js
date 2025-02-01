75225522c6b5b68686a4278096ba650f40a70c97
Bhavrattan Singh Bagga <110535685+rattan004@users.noreply.github.com>
2025-02-01 09:48:19 +0530

    Update productRoute.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addProduct,removeProduct,listProducts,singleProduct } from "../controllers/productController.js";
+import { addProduct,removeProduct,listProducts,singleProduct, modifyStock } from "../controllers/productController.js";
 import upload from "../middleware/multer.js";
 import adminAuth from "../middleware/adminAuth.js";
 
@@ -9,5 +9,6 @@ productRouter.post('/add',adminAuth,upload.fields([{name: 'image1',maxCount:1},{
 productRouter.post('/remove',adminAuth,removeProduct)
 productRouter.post('/single',singleProduct)
 productRouter.get('/list',listProducts)
+productRouter.post('/modify',modifyStock)
 
 export default productRouter
